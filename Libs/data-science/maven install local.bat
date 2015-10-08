91b1426d1388fd637acd671ef6734cc957b10095
tds-andre <tds.andre@gmail.com>
2015-10-08 03:05:38 -0300

    u

---
+++ Libs/data-science/maven install local.bat
@@ -0,0 +1,6 @@
+call mvn install:install-file -Dfile=all\javaml-0.1.5.jar -DgroupId=com.maispartners -DartifactId=ds.javaml -Dversion=0.1.5 -Dpackaging=jar
+call mvn install:install-file -Dfile=all\ajt-1.20.jar -DgroupId=com.maispartners -DartifactId=ds.ajt -Dversion=1.20 -Dpackaging=jar
+call mvn install:install-file -Dfile=all\commons-math-1.2.jar -DgroupId=com.maispartners -DartifactId=ds.commons-math -Dversion=1.2 -Dpackaging=jar
+call mvn install:install-file -Dfile=all\Jama-1.0.2.jar -DgroupId=com.maispartners -DartifactId=ds.Jama -Dversion=1.0.2 -Dpackaging=jar
+call mvn install:install-file -Dfile=all\libsvm.jar -DgroupId=com.maispartners -DartifactId=ds.libsvm -Dversion=na -Dpackaging=jar
+call mvn install:install-file -Dfile=all\weka.jar -DgroupId=com.maispartners -DartifactId=ds.weka -Dversion=na -Dpackaging=jar
